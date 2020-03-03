b1fbf82cdcfe0b130802af67aef0cfb589fbe2c3
Jandan <lutao1096@gmail.com>
2020-03-03 22:46:34 +0800

    update

--- advanced-vuejs/src/components/v-slot/demo-item.vue
+++ advanced-vuejs/src/components/v-slot/demo-item.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <slot name="header" :header="header"></slot>
+      <slot name="header" :sendHeader="header">{{ header }}</slot>
     </div>
     <div class="card-body">
-      <slot :list="list"></slot>
+      <!-- 注意这里的 :sendList="list" , sendList被称为插槽prop,名字可以随意取,表示要传递过去的值 -->
+      <slot :sendList="list">我是DemoItem组件中的default插槽后备内容</slot>
     </div>
     <div class="card-footer">
-      <slot name="footer" :footer="footer"></slot>
+      <slot name="footer" :sendFooter="message">{{ footer }}</slot>
     </div>
   </div>
 </template>
@@ -15,11 +16,29 @@
 <script>
 export default {
   name: 'DemoItem',
+  props: ['message'],
   data() {
     return {
-      header: '我是子组件的头部',
-      footer: '我是子组件的底部',
-      list: [1, 2, 3]
+      header: '我是DemoItem组件中的header插槽后备内容',
+      footer: '我是DemoItem组件中的footer插槽后备内容',
+      list: [
+        {
+          id: 10,
+          text: '苹果'
+        },
+        {
+          id: 20,
+          text: '香蕉'
+        },
+        {
+          id: 30,
+          text: '梨'
+        },
+        {
+          id: 40,
+          text: '芒果'
+        }
+      ]
     }
   }
 }
--- advanced-vuejs/src/components/v-slot/demo.vue
+++ advanced-vuejs/src/components/v-slot/demo.vue
@@ -1,46 +1,39 @@
 <template>
   <div class="mt-3">
-    <!-- 具名插槽 第一种写法 -->
-    <!-- <demo-item>
-      <template v-slot:header>
-        我是头部
-      </template>
-      <div>我是默认slot的内容</div>
-      <template v-slot:footer>
-        我是底部
-      </template>
-    </demo-item> -->
-
-    <!-- 具名插槽 第二种写法 -->
-    <!-- <demo-item>
-      <template #header>
-        我是头部
-      </template>
-      <div>我是默认slot的内容</div>
-      <template #footer>
-        我是底部
-      </template>
-    </demo-item> -->
-
-    <!-- 作用域插槽 -->
+    <h3>后备内容</h3>
     <demo-item>
-      <template #header="{header}">
-        {{ header }}
+      <template v-slot:header></template>
+      <template v-slot:footer></template>
+    </demo-item>
+    <br />
+    <h3>具名插槽缩写</h3>
+    <demo-item>
+      {{ defaultSlot }}
+      <template #header>{{ headerSlot }}</template>
+      <template #footer>{{ footerSlot }}</template>
+    </demo-item>
+    <br />
+    <h3>作用域插槽</h3>
+    <demo-item :message="footerSlot">
+      <!-- 动态插槽名,方括号中不能存在引号和空格 -->
+      <template #[attr]="{sendHeader}">
+        {{ sendHeader }}
       </template>
-      <template #default="{list}">
-        <div>我是默认slot的内容</div>
+      <!-- 这里使用data来接收子组件传过来的sendList,即 data={sendList} -->
+      <template #default="data">
         <ul class="list-group">
           <li
-            v-for="(item, index) in list"
+            v-for="(item, index) in data.sendList"
             :key="index"
             class="list-group-item"
           >
-            {{ item }}
+            {{ item.text }}
           </li>
         </ul>
       </template>
-      <template #footer="{footer}">
-        {{ footer }}
+      <!-- 这里直接解构子组件传过来的sendFooter -->
+      <template #footer="{sendFooter}">
+        {{ sendFooter }}
       </template>
     </demo-item>
   </div>
@@ -52,6 +45,14 @@ export default {
   name: 'VSlot',
   components: {
     demoItem
+  },
+  data() {
+    return {
+      headerSlot: '我会覆盖DemoItem组件中的header插槽',
+      defaultSlot: '我会覆盖DemoItem组件中的default插槽',
+      footerSlot: '我会覆盖DemoItem组件中的footer插槽',
+      attr: 'header'
+    }
   }
 }
 </script>
